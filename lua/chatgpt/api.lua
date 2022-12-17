682591cb5936a8d86a62a42977b48f8ec303a033
Lech Twaróg <lech.twarog@gmail.com>
2022-12-17 14:09:03 +0100

    fix(api): error handling when calling API
    Refactor API module to allow reuse code and fix error handling
    
    fix #10

--- lua/chatgpt/api.lua
+++ lua/chatgpt/api.lua
@@ -4,7 +4,7 @@ local Config = require("chatgpt.config")
 local Api = {}
 
 -- API URL
-Api.URL = "https://api.openai.com/v1/completions"
+Api.COMPLETIONS_URL = "https://api.openai.com/v1/completions"
 
 -- API KEY
 Api.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -14,12 +14,15 @@ end
 
 function Api.completions(prompt, cb)
   local params = vim.tbl_extend("keep", { prompt = prompt }, Config.options.openai_params)
+  Api.make_call(Api.COMPLETIONS_URL, params, cb)
+end
 
+function Api.make_call(url, params, cb)
   Api.job = job
     :new({
       command = "curl",
       args = {
-        Api.URL,
+        url,
         "-H",
         "Content-Type: application/json",
         "-H",
@@ -27,29 +30,34 @@ function Api.completions(prompt, cb)
         "-d",
         vim.fn.json_encode(params),
       },
-      on_exit = vim.schedule_wrap(function(j, exit_code)
-        if exit_code ~= 0 then
-          -- TODO: better error handling
-          vim.notify("An Error Occurred, cannot fetch answer ...", vim.log.levels.ERROR)
-          cb("ERROR: API Error")
-        end
-
-        local res = table.concat(j:result(), "\n")
-        local json = vim.fn.json_decode(res)
-        if json == nil then
-          cb("No Response.")
-        else
-          local response = json.choices[1].text
-          if type(response) == "string" and response ~= "" then
-            cb(response)
-          else
-            cb("...")
-          end
-        end
+      on_exit = vim.schedule_wrap(function(response, exit_code)
+        Api.handle_response(response, exit_code, cb)
       end),
     })
     :start()
 end
+
+Api.handle_response = vim.schedule_wrap(function(responce, exit_code, cb)
+  if exit_code ~= 0 then
+    vim.notify("An Error Occurred ...", vim.log.levels.ERROR)
+    cb("ERROR: API Error")
+  end
+
+  local result = table.concat(responce:result(), "\n")
+  local json = vim.fn.json_decode(result)
+  if json == nil then
+    cb("No Response.")
+  elseif json.error then
+    cb("// API ERROR: " .. json.error.message)
+  else
+    local response = json.choices[1].text
+    if type(response) == "string" and response ~= "" then
+      cb(response)
+    else
+      cb("...")
+    end
+  end
+end)
 
 function Api.close()
   if Api.job then
