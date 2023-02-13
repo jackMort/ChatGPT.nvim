3f6fd348df53b9d15aa0a58709562cf0a3b4636a
Zeng <76579810+Bekaboo@users.noreply.github.com>
2023-02-13 11:43:28 -0600

    feat(hlgroups): do not override existing hlgroups

--- lua/chatgpt.lua
+++ lua/chatgpt.lua
@@ -7,11 +7,11 @@ local M = {}
 
 M.setup = function(options)
   -- set custom highlights
-  vim.api.nvim_set_hl(0, "ChatGPTQuestion", { fg = "#b4befe", italic = true, bold = false })
-  vim.api.nvim_set_hl(0, "ChatGPTWelcome", { fg = "#9399b2", italic = true, bold = false })
-  vim.api.nvim_set_hl(0, "ChatGPTTotalTokens", { fg = "#ffffff", bg = "#444444" })
-  vim.api.nvim_set_hl(0, "ChatGPTTotalTokensBorder", { fg = "#444444" })
-  vim.api.nvim_set_hl(0, "ChatGPTCompletion", { fg = "#9399b2", italic = true, bold = false })
+  vim.api.nvim_set_hl(0, "ChatGPTQuestion", { fg = "#b4befe", italic = true, bold = false, default = true })
+  vim.api.nvim_set_hl(0, "ChatGPTWelcome", { fg = "#9399b2", italic = true, bold = false, default = true })
+  vim.api.nvim_set_hl(0, "ChatGPTTotalTokens", { fg = "#ffffff", bg = "#444444", default = true })
+  vim.api.nvim_set_hl(0, "ChatGPTTotalTokensBorder", { fg = "#444444", default = true })
+  vim.api.nvim_set_hl(0, "ChatGPTCompletion", { fg = "#9399b2", italic = true, bold = false, default = true })
 
   config.setup(options)
   signs.setup()
