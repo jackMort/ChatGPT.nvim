jackMort/ChatGPT.nvim

commit 1f69ba297388bccc604a76d9c940cb26156c5cd1

feat: help panel in chat and edit (#336)
* added config for help

added a toggle_help keybind and help window settings

* copied and uncommented settings.lua

* added help toggle

added a way to easily add extra windows to the right and added the help toggle

* help now displays help

* help panel contened added
* fix not being able to switch between panels

* fixed bug with panel switching

* panel switching more reliable

* fixed freez issue

the freez was accuring while switching panels or toggling one on

* added different help content

help content for
* edits
* chat

* added more toggle binds in chat

bind for
* session
* help

* added help in chat support

* removed never executed code part

* readde cycle panes support

* updater README to list new keybinds

* fix: make sure last panel grow to fill the remaining space

* fix: make sure last panel in chat fills the remaing space

* feat: style a bit the help panel

* style: reformat code

---------

Co-authored-by: Moritz Gleissner <moritz@gleisser.de>
Co-authored-by: Lech Twaróg <lech.twarog@gmail.com>

lua/chatgpt/code_edits.lua
@@ -9,6 +9,7 @@ local Config = require("chatgpt.config")
 local Utils = require("chatgpt.utils")
 local Spinner = require("chatgpt.spinner")
 local Settings = require("chatgpt.settings")
+local Help = require("chatgpt.help")
 
 EDIT_FUNCTION_ARGUMENTS = {
   function_call = {
@@ -140,6 +141,9 @@ M.edit_with_instructions = function(output_lines, bufnr, selection, ...)
   local openai_params = Config.options.openai_edit_params
   local use_functions_for_edits = Config.options.use_openai_functions_for_edits
   local settings_panel = Settings.get_settings_panel("edits", openai_params)
+  local help_panel = Help.get_help_panel("edit") -- I like the highlighting for Lua.
+  local open_extra_panels = {} -- tracks which extra panels are open
+  local active_panel = instructions_input -- for cycling windows
   input_window = Popup(Config.options.popup_window)
   output_window = Popup(Config.options.popup_window)
   instructions_input = ChatInput(Config.options.popup_input, {
@@ -226,56 +230,119 @@ M.edit_with_instructions = function(output_lines, bufnr, selection, ...)
     end, { noremap = true })
   end
 
+  local function inTable(tbl, item)
+    for key, value in pairs(tbl) do
+      if value == item then
+        return key
+      end
+    end
+    return false
+  end
+
+  -- toggle extra panels
+  local function toggle_extra_panel(extra_panel, modifiable_panel)
+    local extra_open = inTable(open_extra_panels, extra_panel)
+    if not extra_open then
+      table.insert(open_extra_panels, extra_panel)
+      local extra_boxes = function()
+        local box_size = (100 / #open_extra_panels) .. "%"
+        local boxes = {}
+        for i, panel in ipairs(open_extra_panels) do
+          -- for the last panel, make it grow to fill the remaining space
+          if i == #open_extra_panels then
+            table.insert(boxes, Layout.Box(panel, { grow = 1 }))
+          else
+            table.insert(boxes, Layout.Box(panel, { size = box_size }))
+          end
+        end
+        return Layout.Box(boxes, { dir = "col", size = 40 })
+      end
+      layout:update(Layout.Box({
+        Layout.Box({
+          Layout.Box(input_window, { grow = 1 }),
+          Layout.Box(instructions_input, { size = 3 }),
+        }, { dir = "col", grow = 1 }),
+        Layout.Box(output_window, { grow = 1 }),
+        extra_boxes(),
+      }, { dir = "row" }))
+      extra_panel:show()
+      extra_panel:mount()
+
+      vim.api.nvim_set_current_win(extra_panel.winid)
+      active_panel = extra_panel
+      vim.api.nvim_buf_set_option(extra_panel.bufnr, "modifiable", modifiable_panel)
+      vim.api.nvim_win_set_option(extra_panel.winid, "cursorline", true)
+    else
+      table.remove(open_extra_panels, extra_open)
+      if #open_extra_panels == 0 then
+        layout:update(Layout.Box({
+          Layout.Box({
+            Layout.Box(input_window, { grow = 1 }),
+            Layout.Box(instructions_input, { size = 3 }),
+          }, { dir = "col", size = "50%" }),
+          Layout.Box(output_window, { size = "50%" }),
+        }, { dir = "row" }))
+        extra_panel:hide()
+        vim.api.nvim_set_current_win(instructions_input.winid)
+        active_panel = instructions_input
+      else
+        local box_size = (100 / #open_extra_panels) .. "%"
+        local extra_boxes = function()
+          local boxes = {}
+          for _, panel in ipairs(open_extra_panels) do
+            table.insert(boxes, Layout.Box(panel, { size = box_size }))
+          end
+          return Layout.Box(boxes, { dir = "col", size = 40 })
+        end
+        layout:update(Layout.Box({
+          Layout.Box({
+            Layout.Box(input_window, { grow = 1 }),
+            Layout.Box(instructions_input, { size = 3 }),
+          }, { dir = "col", grow = 1 }),
+          Layout.Box(output_window, { grow = 1 }),
+          extra_boxes(),
+        }, { dir = "row" }))
+        extra_panel:hide()
+        vim.api.nvim_set_current_win(open_extra_panels[#open_extra_panels].winid)
+        active_panel = open_extra_panels[#open_extra_panels]
+      end
+    end
+    for _, window in ipairs({ input_window, output_window }) do
+      vim.api.nvim_buf_set_option(window.bufnr, "filetype", filetype)
+      vim.api.nvim_win_set_option(window.winid, "number", true)
+    end
+  end
+
   -- toggle settings
-  local settings_open = false
-  for _, popup in ipairs({ settings_panel, instructions_input }) do
+  for _, popup in ipairs({ instructions_input, settings_panel, help_panel }) do
     for _, mode in ipairs({ "n", "i" }) do
       popup:map(mode, Config.options.edit_with_instructions.keymaps.toggle_settings, function()
-        if settings_open then
-          layout:update(Layout.Box({
-            Layout.Box({
-              Layout.Box(input_window, { grow = 1 }),
-              Layout.Box(instructions_input, { size = 3 }),
-            }, { dir = "col", size = "50%" }),
-            Layout.Box(output_window, { size = "50%" }),
-          }, { dir = "row" }))
-          settings_panel:hide()
-          vim.api.nvim_set_current_win(instructions_input.winid)
-        else
-          layout:update(Layout.Box({
-            Layout.Box({
-              Layout.Box(input_window, { grow = 1 }),
-              Layout.Box(instructions_input, { size = 3 }),
-            }, { dir = "col", grow = 1 }),
-            Layout.Box(output_window, { grow = 1 }),
-            Layout.Box(settings_panel, { size = 40 }),
-          }, { dir = "row" }))
-          settings_panel:show()
-          settings_panel:mount()
-
-          vim.api.nvim_set_current_win(settings_panel.winid)
-          vim.api.nvim_buf_set_option(settings_panel.bufnr, "modifiable", false)
-          vim.api.nvim_win_set_option(settings_panel.winid, "cursorline", true)
-        end
-        settings_open = not settings_open
+        toggle_extra_panel(settings_panel, false)
         -- set input and output settings
         --  TODO
-        for _, window in ipairs({ input_window, output_window }) do
-          vim.api.nvim_buf_set_option(window.bufnr, "filetype", filetype)
-          vim.api.nvim_win_set_option(window.winid, "number", true)
-        end
+      end, {})
+    end
+  end
+
+  -- toggle help
+  for _, popup in ipairs({ instructions_input, settings_panel, help_panel }) do
+    for _, mode in ipairs({ "n", "i" }) do
+      popup:map(mode, Config.options.edit_with_instructions.keymaps.toggle_help, function()
+        toggle_extra_panel(help_panel, false)
+        -- set input and output settings
+        --  TODO
       end, {})
     end
   end
 
   -- cycle windows
-  local active_panel = instructions_input
-  for _, popup in ipairs({ input_window, output_window, settings_panel, instructions_input }) do
+  for _, popup in ipairs({ input_window, output_window, settings_panel, help_panel, instructions_input }) do
     for _, mode in ipairs({ "n", "i" }) do
       if mode == "i" and (popup == input_window or popup == output_window) then
         goto continue
       end
       popup:map(mode, Config.options.edit_with_instructions.keymaps.cycle_windows, function()
+        local in_table = inTable(open_extra_panels, active_panel)
         if active_panel == instructions_input then
           vim.api.nvim_set_current_win(input_window.winid)
           active_panel = input_window
@@ -285,16 +352,23 @@ M.edit_with_instructions = function(output_lines, bufnr, selection, ...)
           active_panel = output_window
           vim.api.nvim_command("stopinsert")
         elseif active_panel == output_window and mode ~= "i" then
-          if settings_open then
-            vim.api.nvim_set_current_win(settings_panel.winid)
-            active_panel = settings_panel
-          else
+          if #open_extra_panels == 0 then
             vim.api.nvim_set_current_win(instructions_input.winid)
             active_panel = instructions_input
+          else
+            vim.api.nvim_set_current_win(open_extra_panels[1].winid)
+            active_panel = open_extra_panels[1]
           end
-        elseif active_panel == settings_panel then
-          vim.api.nvim_set_current_win(instructions_input.winid)
-          active_panel = instructions_input
+        elseif in_table then
+          -- next index with wrap around and 0 for instructions_input
+          local next_index = (in_table + 1) % (#open_extra_panels + 1)
+          if next_index == 0 then
+            vim.api.nvim_set_current_win(instructions_input.winid)
+            active_panel = instructions_input
+          else
+            vim.api.nvim_set_current_win(open_extra_panels[next_index].winid)
+            active_panel = open_extra_panels[next_index]
+          end
         end
       end, {})
       ::continue::
@@ -303,7 +377,7 @@ M.edit_with_instructions = function(output_lines, bufnr, selection, ...)
 
   -- toggle diff mode
   local diff_mode = Config.options.edit_with_instructions.diff
-  for _, popup in ipairs({ settings_panel, instructions_input }) do
+  for _, popup in ipairs({ help_panel, settings_panel, instructions_input }) do
     for _, mode in ipairs({ "n", "i" }) do
       popup:map(mode, Config.options.edit_with_instructions.keymaps.toggle_diff, function()
         diff_mode = not diff_mode

lua/chatgpt/config.lua
@@ -17,6 +17,7 @@ function M.defaults()
         accept = "<C-y>",
         toggle_diff = "<C-d>",
         toggle_settings = "<C-o>",
+        toggle_help = "<C-h>",
         cycle_windows = "<Tab>",
         use_output_as_input = "<C-i>",
       },
@@ -44,7 +45,7 @@ function M.defaults()
         },
       },
       keymaps = {
-        close = { "<C-c>" },
+        close = "<C-c>",
         yank_last = "<C-y>",
         yank_last_code = "<C-k>",
         scroll_up = "<C-u>",
@@ -61,6 +62,8 @@ function M.defaults()
         edit_message = "e",
         delete_message = "d",
         toggle_settings = "<C-o>",
+        toggle_sessions = "<C-p>",
+        toggle_help = "<C-h>",
         toggle_message_role = "<C-r>",
         toggle_system_role_open = "<C-s>",
         stop_generating = "<C-x>",
@@ -139,6 +142,18 @@ function M.defaults()
         winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
       },
     },
+    help_window = {
+      setting_sign = "  ",
+      border = {
+        style = "rounded",
+        text = {
+          top = " Help ",
+        },
+      },
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+    },
     openai_params = {
       model = "gpt-3.5-turbo",
       frequency_penalty = 0,
@@ -160,6 +175,10 @@ function M.defaults()
     actions_paths = {},
     show_quickfixes_cmd = "Trouble quickfix",
     predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+    highlights = {
+      help_key = "@symbol",
+      help_description = "@comment",
+    },
   }
   return defaults
 end

lua/chatgpt/flows/chat/base.lua
@@ -6,6 +6,7 @@ local ChatInput = require("chatgpt.input")
 local Api = require("chatgpt.api")
 local Config = require("chatgpt.config")
 local Settings = require("chatgpt.settings")
+local Help = require("chatgpt.help")
 local Sessions = require("chatgpt.flows.chat.sessions")
 local Utils = require("chatgpt.utils")
 local Signs = require("chatgpt.signs")
@@ -35,7 +36,9 @@ function Chat:init()
   self.chat_input = nil
   self.chat_window = nil
   self.sessions_panel = nil
+  self.open_extra_panels = {} -- track open panels
   self.settings_panel = nil
+  self.help_panel = nil
   self.system_role_panel = nil
 
   -- UI OPEN INDICATORS
@@ -594,7 +597,14 @@ end
 
 function Chat:map(keys, fn, windows, modes)
   if windows == nil or next(windows) == nil then
-    windows = { self.settings_panel, self.sessions_panel, self.system_role_panel, self.chat_input, self.chat_window }
+    windows = {
+      self.help_panel,
+      self.settings_panel,
+      self.sessions_panel,
+      self.system_role_panel,
+      self.chat_input,
+      self.chat_window,
+    }
   end
 
   if modes == nil or next(modes) == nil then
@@ -637,7 +647,7 @@ function Chat:get_layout_params()
   local starting_row = tabline_height == 0 and 0 or 1
 
   local width = Utils.calculate_percentage_width(Config.options.popup_layout.right.width)
-  if self.settings_open then
+  if #self.open_extra_panels > 0 then
     width = width + 40
   end
 
@@ -677,7 +687,20 @@ function Chat:get_layout_params()
     Layout.Box(self.chat_input, { size = (self.chat_input.border._.style == "none" and 0 or 2) + self.prompt_lines }),
   }, { dir = "col" })
 
-  if self.settings_open then
+  if #self.open_extra_panels > 0 then
+    local extra_boxes = function()
+      local box_size = (100 / #self.open_extra_panels) .. "%"
+      local boxes = {}
+      for i, panel in ipairs(self.open_extra_panels) do
+        -- for the last panel, make it grow to fill the remaining space
+        if i == #self.open_extra_panels then
+          table.insert(boxes, Layout.Box(panel, { grow = 1 }))
+        else
+          table.insert(boxes, Layout.Box(panel, { size = box_size }))
+        end
+      end
+      return Layout.Box(boxes, { dir = "col", size = 40 })
+    end
     box = Layout.Box({
       Layout.Box({
         left_layout,
@@ -686,10 +709,7 @@ function Chat:get_layout_params()
           { size = (self.chat_input.border._.style == "none" and 0 or 2) + self.prompt_lines }
         ),
       }, { dir = "col", grow = 1 }),
-      Layout.Box({
-        Layout.Box(self.settings_panel, { size = "30%" }),
-        Layout.Box(self.sessions_panel, { grow = 1 }),
-      }, { dir = "col", size = 40 }),
+      extra_boxes(),
     }, { dir = "row" })
   end
 
@@ -698,6 +718,7 @@ end
 
 function Chat:open()
   self.settings_panel = Settings.get_settings_panel("chat_completions", self.params)
+  self.help_panel = Help.get_help_panel("chat")
   self.sessions_panel = Sessions.get_panel(function(session)
     self:set_session(session)
   end)
@@ -801,35 +822,83 @@ function Chat:open()
     end
   end)
 
+  local function inTable(tbl, item)
+    for key, value in pairs(tbl) do
+      if value == item then
+        return key
+      end
+    end
+    return false
+  end
+
   -- toggle settings
   self:map(Config.options.chat.keymaps.toggle_settings, function()
-    self.settings_open = not self.settings_open
+    local settings_open = inTable(self.open_extra_panels, self.settings_panel)
+    if settings_open then
+      table.remove(self.open_extra_panels, settings_open)
+      settings_open = false
+    else
+      table.insert(self.open_extra_panels, self.settings_panel)
+      settings_open = inTable(self.open_extra_panels, self.settings_panel)
+    end
     self:redraw()
 
-    if self.settings_open then
-      vim.api.nvim_buf_set_option(self.settings_panel.bufnr, "modifiable", false)
-      vim.api.nvim_win_set_option(self.settings_panel.winid, "cursorline", true)
+    if settings_open then
+      vim.api.nvim_buf_set_option(self.open_extra_panels[settings_open].bufnr, "modifiable", false)
+      vim.api.nvim_win_set_option(self.open_extra_panels[settings_open].winid, "cursorline", true)
 
-      self:set_active_panel(self.settings_panel)
+      self:set_active_panel(self.open_extra_panels[settings_open])
     else
       self:set_active_panel(self.chat_input)
     end
+  end)
+
+  -- toggle help
+  self:map(Config.options.chat.keymaps.toggle_help, function()
+    local help_open = inTable(self.open_extra_panels, self.help_panel)
+    if help_open then
+      table.remove(self.open_extra_panels, help_open)
+      help_open = false
+    else
+      table.insert(self.open_extra_panels, self.help_panel)
+      help_open = inTable(self.open_extra_panels, self.help_panel)
+    end
+    self:redraw()
+
+    if help_open then
+      vim.api.nvim_buf_set_option(self.open_extra_panels[help_open].bufnr, "modifiable", false)
+      vim.api.nvim_win_set_option(self.open_extra_panels[help_open].winid, "cursorline", true)
+
+      self:set_active_panel(self.help_panel)
+    else
+      self:set_active_panel(self.chat_input)
+    end
+  end)
+
+  -- toggle sessions
+  self:map(Config.options.chat.keymaps.toggle_sessions, function()
+    local sessions_open = inTable(self.open_extra_panels, self.sessions_panel)
+    if sessions_open then
+      table.remove(self.open_extra_panels, sessions_open)
+    else
+      table.insert(self.open_extra_panels, self.sessions_panel)
+    end
+    self:redraw()
   end)
 
   -- new session
   self:map(Config.options.chat.keymaps.new_session, function()
     self:new_session()
     Sessions:refresh()
-  end, { self.settings_panel, self.chat_input })
+  end, { self.settings_panel, self.chat_input, self.help_panel })
 
   -- cycle panes
   self:map(Config.options.chat.keymaps.cycle_windows, function()
+    local in_table = inTable(self.open_extra_panels, self.active_panel)
     if not self.active_panel then
       self:set_active_panel(self.chat_input)
     end
-    if self.active_panel == self.settings_panel then
-      self:set_active_panel(self.sessions_panel)
-    elseif self.active_panel == self.chat_input then
+    if self.active_panel == self.chat_input then
       if self.system_role_open then
         self:set_active_panel(self.system_role_panel)
       else
@@ -837,8 +906,19 @@ function Chat:open()
       end
     elseif self.active_panel == self.system_role_panel then
       self:set_active_panel(self.chat_window)
-    elseif self.active_panel == self.chat_window and self.settings_open == true then
-      self:set_active_panel(self.settings_panel)
+    elseif self.active_panel == self.chat_window then
+      if #self.open_extra_panels > 0 then
+        self:set_active_panel(self.open_extra_panels[1])
+      else
+        self:set_active_panel(self.chat_input)
+      end
+    elseif in_table then
+      local next_index = (in_table + 1) % (#self.open_extra_panels + 1)
+      if next_index == 0 then
+        self:set_active_panel(self.chat_input)
+      else
+        self:set_active_panel(self.open_extra_panels[next_index])
+      end
     else
       self:set_active_panel(self.chat_input)
     end

lua/chatgpt/help.lua
@@ -1,0 +1,60 @@
+local M = {}
+M.vts = {}
+
+local NuiLine = require("nui.line")
+local NuiText = require("nui.text")
+local Popup = require("nui.popup")
+local Config = require("chatgpt.config")
+
+M.get_help_panel = function(type)
+  M.type = type
+  M.panel = Popup(Config.options.help_window)
+
+  local line = 1
+
+  local settings
+  if M.type == "edit" then
+    settings = Config.options.edit_with_instructions.keymaps
+  elseif M.type == "chat" then
+    settings = Config.options.chat.keymaps
+  else
+    settings = {}
+  end
+
+  -- sort alphabetically by keys for consistency
+  local settings_keys = {}
+  -- populate the table that holds the keys
+  for k in pairs(settings) do
+    table.insert(settings_keys, k)
+  end
+  -- sort the keys
+  table.sort(settings_keys)
+
+  -- find the longest setting value
+  local max_setting_value_length = 0
+  for _, k in pairs(settings_keys) do
+    if #settings[k] > max_setting_value_length then
+      max_setting_value_length = #settings[k]
+    end
+  end
+
+  for _, k in pairs(settings_keys) do
+    local key = NuiText(
+      " " .. settings[k] .. string.rep(" ", max_setting_value_length + 1 - #settings[k]),
+      Config.options.highlights.help_key
+    )
+
+    -- make desciption human readable by replacing _ with space
+    local description = k:gsub("_", " ")
+
+    local value = NuiText(description, Config.options.highlights.help_description)
+    local line_text = NuiLine({ key, value })
+
+    line_text:render(M.panel.bufnr, -1, line)
+    line = line + 1
+  end
+
+  return M.panel
+end
+
+return M

commit cb193185f9539d74af44d32d01d615c6187bda2f

Fix issue with cycling panes in edit_with_instructions
This Fixes https://github.com/jackMort/ChatGPT.nvim/issues/352

Closes #352

Something in 1f69ba2 broke this - here's a fix for it.

For some reason the active_panel is coming up nil when the view
initially opens. It's being set at the top, but something is overwriting
it.

lua/chatgpt/code_edits.lua
@@ -352,6 +352,7 @@ M.edit_with_instructions = function(output_lines, bufnr, selection, ...)
         goto continue
       end
       popup:map(mode, Config.options.edit_with_instructions.keymaps.cycle_windows, function()
+        active_panel = active_panel or instructions_input -- otherwise active_panel is nil when window opens
         local in_table = inTable(open_extra_panels, active_panel)
         if active_panel == instructions_input then
           vim.api.nvim_set_current_win(input_window.winid)