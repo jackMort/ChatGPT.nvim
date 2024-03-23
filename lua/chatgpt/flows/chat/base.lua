2240812afd410c1d382b17deddc0978652b9407b
barnii77 <david.harrer@gmx.at>
2024-03-23 20:05:36 +0100

    debugging

--- lua/chatgpt/flows/chat/base.lua
+++ lua/chatgpt/flows/chat/base.lua
@@ -737,6 +737,7 @@ end
 
 function Chat:open()
   local openai_params = Utils.collapsed_openai_params(self.params)
+  vim.notify(vim.inspect(openai_params))
   self.settings_panel = Settings.get_settings_panel("chat_completions", openai_params)
   self.help_panel = Help.get_help_panel("chat")
   self.sessions_panel = Sessions.get_panel(function(session)
